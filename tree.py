5fca0bfb038615e4b124f37081259301a2afeb8b
langerss <45015366+langerss@users.noreply.github.com>
2018-12-28 16:13:49 +0000

    Add files via upload
    Adding colours to the tree
    Adding function to create a classic Christmas tree shape
    Swapped to using classic Christmas tree function

--- tree.py
+++ tree.py
@@ -1,5 +1,6 @@
 import random
-
+from colorama import init
+from colorama import Fore, Back, Style
 char = [ "@", "#", "*"]
 bits = ["0", "1"]
 tree_top_one = "*"
@@ -29,7 +30,7 @@ def string_of_spaces(length):
         space_char += " "
     return space_char
 
-def calc_spaces(line_num):
+def generate_spaces(line_num):
     space_count = size_tree - line_num
     return string_of_spaces(space_count)
 
@@ -45,24 +46,40 @@ def check_prime(test_num):
         test_count += 1
     return True
 
-def calc_bits(line_num):
+def calc_bits_len(line_num):
     return line_num * 2 + 1
 
 def build_tree():
     line = 1
     while line <= size_tree:
-        num_bits = calc_bits(line)
-        tree_line = calc_spaces(line) + random_string_gen(num_bits,bits)
+        num_bits = calc_bits_len(line)
+        tree_line = generate_spaces(line) + random_string_gen(num_bits,bits)
         tree_line = prime_position(tree_line,line)
+        tree_line = set_colour(tree_line)
         print(tree_line)
         line +=1
     return
 
+def build_classic_tree():
+    line = 1
+    tree_line_len = line
+    while line <= size_tree:
+        if should_shorten_line(line) == True:
+            tree_line_len -= 3
+        num_bits = calc_bits_len(tree_line_len)
+        tree_line = generate_spaces(tree_line_len) + random_string_gen(num_bits,bits)
+        tree_line = prime_position(tree_line,tree_line_len)
+        tree_line = set_colour(tree_line)
+        print(tree_line)
+        line +=1
+        tree_line_len += 1
+    return
+
 def build_topper(line):
     if line == 1:
-        print(calc_spaces(line-1) + tree_top_one)
+        print(Fore.YELLOW + generate_spaces(line-1) + tree_top_one + Style.RESET_ALL)
     if line == 2:
-        print(calc_spaces(line-1) + tree_top_two)
+        print(Fore.YELLOW + generate_spaces(line-1) + tree_top_two + Style.RESET_ALL)
     return
 
 def prime_position(string,line_num):
@@ -79,7 +96,23 @@ def prime_position(string,line_num):
             char_num +=1
     return new_string
 
+def set_colour(string):
+    new_string = ""
+    for item in string:
+        if item in bits:
+            new_string = new_string + Fore.GREEN + item + Style.RESET_ALL
+        elif item in char:
+            new_string = new_string + Fore.RED + item + Style.RESET_ALL
+        else:
+            new_string = new_string + item
+    return new_string
 
+def should_shorten_line(line_num):
+    result = (line_num % 6 == 0)
+    return result
+
+init()
 build_topper(1)
 build_topper(2)
-build_tree()
+build_classic_tree()
+
